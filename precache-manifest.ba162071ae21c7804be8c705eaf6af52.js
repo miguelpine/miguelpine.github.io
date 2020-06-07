9d51922efcc16f4b9aaac4d1ec99c28a4491bb1f
Miguel P. Rodrigues <miguel.pinr@gmail.com>
2020-06-08 00:02:39 +0100

    version 1.0.0

---
+++ precache-manifest.ba162071ae21c7804be8c705eaf6af52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e94fae54ca33e3afbb06c8b6790ecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "840fd98b63689a5dabf1",
+    "url": "/static/css/main.c35aa56f.chunk.css"
+  },
+  {
+    "revision": "0a8a4ac211addd7a36ca",
+    "url": "/static/js/2.0a55cdc3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0a55cdc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840fd98b63689a5dabf1",
+    "url": "/static/js/main.566af7a1.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
